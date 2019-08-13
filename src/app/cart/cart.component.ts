c28562e522b922dd4ca2f26fbecfb21146033c15
tigervanilla <piyushcool102@gmail.com>
2019-08-13 12:20:34 +0530

    remove item from cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -11,19 +11,41 @@ import { summaryFileName } from '@angular/compiler/src/aot/util';
 export class CartComponent implements OnInit {
   cartItems: any;
   amountPayable: number;
+  name: string;
+  email: string;
+  mobile: string;
+  address: string;
+  pincode: string;
   constructor(private cartService: CartService) { }
+
   ngOnInit() {
     this.cartItems = this.cartService.getItemsInCart();
     console.log(this.cartItems);
     this.getPayableAmount();
   }
 
+  // ngOnDestroy() {
+  //   this.cartService.syncCart(this.cartItems);
+  // }
+
   getPayableAmount(): void {
     this.amountPayable =  0;
     for (const item of this.cartItems) {
       this.amountPayable += item.qty * item.price;
     }
+    this.cartService.syncCart(this.cartItems);
     // return this.amountPayable;
+  }
+
+  removeItemFromCart(index: number): void {
+    console.log(index);
+    this.cartItems.splice(index, 1);
+    this.cartService.syncCart(this.cartItems);
+    this.getPayableAmount();
+  }
+
+  printShippingDetails(): void {
+    console.log(this.name, this.email, this.address, this.mobile, this.pincode);
   }
 
 }
